458b3546689a53edb538f770bb126ae2ffe0cfb2
Alex Korzun <koleksandr4@gmail.com>
2024-03-28 15:19:52 +0200

    changed connect() on hooks useSelector() and useDispatch()

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,33 @@
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
-import * as actions from '../actions';
+import { inc, dec, rnd } from '../actions';
 
-const Counter = ({ counter, inc, dec, rnd }) => {
+const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
+export default Counter;
+
+
+
+// using connect()
+
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.value
+//     }
+// }
 
 // const mapDispatchToProps = (dispatch) => {
 //     return bindActionCreators(actions, dispatch);
@@ -26,4 +36,4 @@ const mapStateToProps = (state) => {
 // If we push an Object as a second arg to the connect(), it will automatically cover all actionCreators
 // with dispatch(), so mapDispatchToProps() is not needed here.
 
-export default connect(mapStateToProps, actions)(Counter);
+// export default connect(mapStateToProps, actions)(Counter);
